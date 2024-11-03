7cb2303dd828af0d66ca5b875c5c6703a6855396
Jayesh <jayeshjaiswal2510@gmail.com>
2024-11-03 10:08:22 +0530

    updates

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// entery point
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -14,6 +13,31 @@ app.get("/getTasks", async (req, res) => {
     res.json(todos);
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+app.put("/update/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedTask = await TaskModel.findByIdAndUpdate(id, { done: true });
+
+    if (!updatedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.json(updatedTask);
+  } catch (error) {
+    res.status(500).json({ error: "Error updating task", details: error });
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updatedTask = await TaskModel.findByIdAndDelete(id);
+
+    res.json(updatedTask);
+  } catch (error) {
+    res.status(500).json({ error: "Error updating task", details: error });
   }
 });
 
@@ -33,7 +57,7 @@ mongoose
     "mongodb+srv://jayeshjaiswal2510:KArAG2FHXEPJM9Vx@tasks.ak1un.mongodb.net/todos?retryWrites=true&w=majority&appName=Tasks"
   )
   .then(() => {
-    console.log("Connected with server");
+    console.log("Connected with Database");
     app.listen(5000, () => {
       console.log("Server runnning on port number 5000 ");
     });
